d0a069ba6c583afb9735048d3cc07ea46135517d
Thomas Patérac <paterac.thomas@gmail.com>
2018-03-28 15:10:05 +0200

    création profil user et photo user

--- app/src/main/java/fr/wildcodeschool/rateeverything/MainActivity.java
+++ app/src/main/java/fr/wildcodeschool/rateeverything/MainActivity.java
@@ -1,7 +1,10 @@
 package fr.wildcodeschool.rateeverything;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button buttonprofiluser = findViewById(R.id.button_profil_user);
+        buttonprofiluser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,ProfilUserActivity.class));
+            }
+        });
+
+        Button buttonphotouser = findViewById(R.id.button_user_photo);
+        buttonphotouser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, UserPhoto.class));
+            }
+        });
+
     }
 }
