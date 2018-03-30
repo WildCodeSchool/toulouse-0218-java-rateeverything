8e9437fd495d6a8c05771526419afe9c00073389
Coralie Delplace <coralie.delplace@gmail.com>
2018-03-30 11:56:18 +0200

    resize

--- app/src/main/java/fr/wildcodeschool/rateeverything/FollowersActivity.java
+++ app/src/main/java/fr/wildcodeschool/rateeverything/FollowersActivity.java
@@ -16,12 +16,12 @@ public class FollowersActivity extends AppCompatActivity {
         final ListView listFollowers = findViewById(R.id.listview_followers);
         ArrayList<FollowersModel> followers = new ArrayList<>();
 
-        followers.add(new FollowersModel(R.drawable.lebosse, 23, 12, "Benjamin W"));
-        followers.add(new FollowersModel(R.drawable.toto, 2, 150, "Thomas"));
-        followers.add(new FollowersModel(R.drawable.coco, 200, 1500, "Coralie"));
-        followers.add(new FollowersModel(R.drawable.benou, 50, 2, "Benjamin B"));
-        followers.add(new FollowersModel(R.drawable.tofperrine, 150, 4, "Perrine"));
-        followers.add(new FollowersModel(R.drawable.pascaltof, 20, 50, "Pascal"));
+        followers.add(new FollowersModel(R.drawable.lebosse_min, 23, 12, "Benjamin W"));
+        followers.add(new FollowersModel(R.drawable.toto_min, 2, 150, "Thomas"));
+        followers.add(new FollowersModel(R.drawable.coco_min, 200, 1500, "Coralie"));
+        followers.add(new FollowersModel(R.drawable.benou_min, 50, 2, "Benjamin B"));
+        followers.add(new FollowersModel(R.drawable.tofperrine_min, 150, 4, "Perrine"));
+        followers.add(new FollowersModel(R.drawable.pascaltof_min, 20, 50, "Pascal"));
 
         FollowersAdapter adapter = new FollowersAdapter(this, followers);
         listFollowers.setAdapter(adapter);
