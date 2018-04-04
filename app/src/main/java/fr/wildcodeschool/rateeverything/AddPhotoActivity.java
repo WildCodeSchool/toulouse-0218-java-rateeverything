353ab5f0b3ab4c55c78529240332d46f964daf4b
Coralie Delplace <coralie.delplace@gmail.com>
2018-04-04 11:21:15 +0200

    Fonctionnalité ajout depuis cam appareil

--- app/src/main/java/fr/wildcodeschool/rateeverything/AddPhotoActivity.java
+++ app/src/main/java/fr/wildcodeschool/rateeverything/AddPhotoActivity.java
@@ -1,13 +1,49 @@
 package fr.wildcodeschool.rateeverything;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.ContactsContract;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
-public class AddPhotoActivity extends AppCompatActivity {
+public class AddPhotoActivity extends Activity {
+    Button buttonAdd;
+    ImageView imagePhoto;
+    static final int CAM_REQUEST = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_photo);
+        buttonAdd =(Button) findViewById(R.id.button_add_photo);
+        imagePhoto =(ImageView) findViewById(R.id.ivPhoto);
+        buttonAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent camera_intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(camera_intent, CAM_REQUEST);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch(requestCode) {
+            case CAM_REQUEST:
+                if(resultCode == RESULT_OK) {
+                    Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+                    imagePhoto.setImageBitmap(bitmap);
+                }
+                break;
+        }
     }
 }
+
+
+
